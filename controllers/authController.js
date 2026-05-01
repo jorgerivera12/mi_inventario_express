ca62badefeadce4b1f0b82d37e41d583d03d38eb
Jorge Rivera <jrivera@tws2.io>
2026-05-01 16:44:25 -0500

    feat: diseño de vistas con Handlebars (login, lista y formularios)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,23 @@
 const { validationResult } = require('express-validator');
 const Usuario = require('../models/Usuario');
 
+const LAYOUT_AUTH = { layout: 'auth' };
+
 exports.formRegistro = (req, res) => {
-  res.render('auth/registro', { title: 'Registro' });
+  res.render('auth/registro', { title: 'Registro', ...LAYOUT_AUTH });
 };
 
 exports.registro = async (req, res) => {
   const errores = validationResult(req);
   if (!errores.isEmpty()) {
-    return res.render('auth/registro', {
-      title: 'Registro',
-      errores: errores.array(),
-      body: req.body
-    });
+    return res.render('auth/registro', { title: 'Registro', ...LAYOUT_AUTH, errores: errores.array(), body: req.body });
   }
 
   const { nombre, email, password } = req.body;
-
   const existe = await Usuario.findOne({ email });
   if (existe) {
     return res.render('auth/registro', {
-      title: 'Registro',
+      title: 'Registro', ...LAYOUT_AUTH,
       errores: [{ msg: 'El email ya está registrado' }],
       body: req.body
     });
@@ -31,37 +28,24 @@ exports.registro = async (req, res) => {
 };
 
 exports.formLogin = (req, res) => {
-  res.render('auth/login', { title: 'Iniciar sesión' });
+  res.render('auth/login', { title: 'Iniciar sesión', ...LAYOUT_AUTH });
 };
 
 exports.login = async (req, res) => {
   const errores = validationResult(req);
   if (!errores.isEmpty()) {
-    return res.render('auth/login', {
-      title: 'Iniciar sesión',
-      errores: errores.array(),
-      body: req.body
-    });
+    return res.render('auth/login', { title: 'Iniciar sesión', ...LAYOUT_AUTH, errores: errores.array(), body: req.body });
   }
 
   const { email, password } = req.body;
-
   const usuario = await Usuario.findOne({ email });
   if (!usuario) {
-    return res.render('auth/login', {
-      title: 'Iniciar sesión',
-      errores: [{ msg: 'Credenciales incorrectas' }],
-      body: req.body
-    });
+    return res.render('auth/login', { title: 'Iniciar sesión', ...LAYOUT_AUTH, errores: [{ msg: 'Credenciales incorrectas' }], body: req.body });
   }
 
   const valido = await usuario.compararPassword(password);
   if (!valido) {
-    return res.render('auth/login', {
-      title: 'Iniciar sesión',
-      errores: [{ msg: 'Credenciales incorrectas' }],
-      body: req.body
-    });
+    return res.render('auth/login', { title: 'Iniciar sesión', ...LAYOUT_AUTH, errores: [{ msg: 'Credenciales incorrectas' }], body: req.body });
   }
 
   req.session.usuarioId = usuario._id;
--- models/Usuario.js
+++ models/Usuario.js
@@ -22,10 +22,9 @@ const usuarioSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash antes de guardar
-usuarioSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+usuarioSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 // Comparar contraseña
